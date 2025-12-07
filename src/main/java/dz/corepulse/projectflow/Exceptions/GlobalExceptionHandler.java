8d9675802731c958d7deb88d98049480cf3fc421
alaa.bourega <alaa.bourega@corepulse.dz>
2025-12-07 02:05:32 +0100

    fix openApi conflict

--- src/main/java/dz/corepulse/projectflow/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/dz/corepulse/projectflow/Exceptions/GlobalExceptionHandler.java
@@ -7,9 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice(basePackages = "dz.corepulse.projectflow.Controllers")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
